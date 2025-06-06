fa615474986f62d9c8322c8d70d4dbba18c6cb49
Thanawat Lerdlumyong <tlerdlumyong@live.com>
2025-06-07 02:34:53 +0700

    refactor(routes): streamline route definitions

--- routes/sensorWidgetRoutes.js
+++ routes/sensorWidgetRoutes.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const { getSensorWidget, createSensorWidget, updateSensorWidget, deleteSensorWidget } = require('../controllers/sensorWidgetController')
+const {
+  getSensorWidget,
+  createSensorWidget,
+  updateSensorWidget,
+  deleteSensorWidget,
+} = require('../controllers/sensorWidgetController');
 
 const router = express.Router();
 
-router.get('/:device_id', getSensorWidget);
+router
+  .route('/:device_id')
+  .get(getSensorWidget)
+  .delete(deleteSensorWidget);
 
-// create a product
 router.post('/', createSensorWidget);
-// update a product
 router.post('/update/:device_id', updateSensorWidget);
-// delete a product
-router.delete('/:device_id', deleteSensorWidget);
 
 module.exports = router;
